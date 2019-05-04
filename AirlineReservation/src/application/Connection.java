cc9fb027c2b4994bf6810a38864086841d41f768
Charles <Charles@10.250.248.69>
2019-05-04 19:17:51 -0400

    Connection

--- AirlineReservation/src/application/Connection.java
+++ AirlineReservation/src/application/Connection.java
@@ -1,22 +1,19 @@
 package application;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Properties;
 
-public class Connection {
-	private static Connection conn;  
-
-	   public static Connection connect() throws SQLException{  
-	     try{  
-	       Class.forName("com.mysql.jdbc.Driver").newInstance();  
-	     }catch(ClassNotFoundException cnfe){  
-	       System.err.println("Error: "+cnfe.getMessage());  
-	     }catch(InstantiationException ie){  
-	       System.err.println("Error: "+ie.getMessage());  
-	     }catch(IllegalAccessException iae){  
-	       System.err.println("Error: "+iae.getMessage());  
-	     }  
-	     conn = (Connection) DriverManager.getConnection(url,user,"");  
-	     return conn;  
-	   }  
-}
+import java.sql.*;  
+public class Connection{  
+public static void main(String args[]){  
+try{  
+Class.forName("com.mysql.jdbc.Driver");  
+java.sql.Connection con= DriverManager.getConnection(  
+"jdbc:mysql://localhost:3306/Database");  
+//creating a connection to the url of where the database is locally stored
+//3306 is the default port
+Statement stmt=con.createStatement();  
+ResultSet rs=stmt.executeQuery("select * from emp");  
+while(rs.next())  
+System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+con.close();  
+}catch(Exception e){ System.out.println(e);}  
+}  
+}  
