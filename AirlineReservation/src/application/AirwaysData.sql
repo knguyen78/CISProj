bd9c88f430d815f41f97ec9111d1bdb70a282c19
Kat Nguyen <knguyen78@student.gsu.edu>
2019-05-05 20:04:39 -0400

    commit

--- AirlineReservation/src/application/AirwaysData.sql
+++ AirlineReservation/src/application/AirwaysData.sql
@@ -1,7 +1,10 @@
-create table registration(
+Create Database If not exists AirwaysData;
+use AirwaysData;
+create table if not exists registration (
 customerID INT Auto_Increment Not Null,
 firstname varchar(50) Not Null,
 lastname varchar(50) Not Null,
+city varchar (50) Not Null,
 address varchar(50) Not Null,
 zip numeric(5) Not Null,
 state varchar(2) Not Null,
@@ -14,7 +17,7 @@ securityAnswer varchar(100) Not Null,
 isAdmin boolean Not Null,
 primary key (customerID)
 );
-create table Flight(
+create table if not exists Flight(
 flightID Int Auto_Increment,
 departureCity varchar (50) Not Null,
 arrivalCity varchar (50) Not Null,
@@ -24,13 +27,13 @@ airplaneid numeric (4) Not Null,
 primary key (flightID)
 );
 
-create table airplane(
+create table if not exists airplane(
 airplaneID Int Auto_Increment Not Null,
 maxCapacity numeric (3) Not Null,
 currentCapacity numeric (3) Not Null,
 primary key (airplaneID)
 );
-create table bookCheck(
+create table if not exists bookCheck(
 BookedID Int Not Null,
 CustomerID Int Not Null,
 FlightID Int Not Null
@@ -60,14 +63,14 @@ Values
 ('Paris', 'New York','8:30:00','11:30:00', 1),
 ('Atlanta', 'Los Angelos','8:30:00','11:30:00', 1);
 
-Insert into registration(firstname, lastname, address, zip, state, username, pword, 
+Insert into registration(firstname, lastname,city, address, zip, state, username, pword, 
 email, SSN, securityQuestion, securityAnswer, isAdmin)
 Values
-('Charles', 'Money', '141 Piedmont AV', '30080','GA','cmoney',
+('Charles', 'Money','Atlanta', '141 Piedmont AV', '30080','GA','cmoney',
 'password','cmoney3@student.gsu.edu',111111111,'What is the Answer', '42',true),
 
-('Nolan', 'Bullard', '141 Piedmont AV', '30080','GA','nbullard',
+('Nolan', 'Bullard','Atlanta', '141 Piedmont AV', '30080','GA','nbullard',
 'password','nolanbullard@gmail.com',222222222,'What is the Answer', '42',true),
 
-('Kat', 'Nguyen', '141 Piedmont AV', '30080','GA','knguyen',
+('Kat', 'Nguyen','Atlanta', '141 Piedmont AV', '30080','GA','knguyen',
 'password','kathy0nguyen@gmail.com',333333333,'What is the Answer', '42',true);
