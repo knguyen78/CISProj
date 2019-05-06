ed1071a8d79a90de6f29b4e4ea3e1c723928de73
Kathy <Kathy@VAL>
2019-05-06 04:14:08 -0400

    added search by button

--- AirlineReservation/src/application/EditFlightsController.java
+++ AirlineReservation/src/application/EditFlightsController.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.*;
 import java.util.ResourceBundle;
@@ -7,12 +8,36 @@ import java.util.ResourceBundle;
 import bLogic.Flight;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
 public class EditFlightsController implements Initializable {
+	
+    @FXML
+    private TextField searchByField;
+	
+	@FXML
+    private Button depCity_btn;
+
+    @FXML
+    private Button arrCity_btn;
+
+    @FXML
+    private Button deptTime_btn;
+
+    @FXML
+    private Button arrTime_btn;
+    
+    @FXML
+    private Button mainMenu_btn;
 	
     @FXML
     private TableView<Flight> flightTable; 
@@ -66,6 +91,155 @@ public class EditFlightsController implements Initializable {
 		
 		
 		
+		
+	}
+	
+	@FXML
+	void setMainMenu(ActionEvent event) {
+		Parent loader;
+		Scene newScene;
+		
+        try {
+			loader = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+			newScene = new Scene(loader);
+			Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+	        
+	        window.setScene(newScene);
+        } catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@FXML
+	void searchByDepartureCity(ActionEvent event) {
+		String search = searchByField.getText();
+		flightTable.getItems().clear();
+		
+		try {
+			Connection c = DBConnect.connect();
+			
+			ResultSet rs = c.createStatement().executeQuery("SELECT * from AirwaysData.flight WHERE departureCity LIKE '"+ search+"%'");
+			
+			while (rs.next()) {
+				flightList.add(new Flight(rs.getInt("flightID"), 
+						rs.getString("departureCity"), rs.getString("arrivalCity"), 
+						rs.getTime("departureTime"), rs.getTime("arrivalTime")));
+			}
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+		col_flightID.setCellValueFactory(new PropertyValueFactory<>("id"));
+		col_DepCity.setCellValueFactory(new PropertyValueFactory<>("depCity"));
+		col_ArrCity.setCellValueFactory(new PropertyValueFactory<>("arrCity"));
+		col_DepTime.setCellValueFactory(new PropertyValueFactory<>("depTime"));
+		col_ArrTime.setCellValueFactory(new PropertyValueFactory<>("arrTime"));
+		
+		flightTable.setItems(flightList);
+		
+	}
+	
+	@FXML
+	void searchByDepartureTime(ActionEvent event) {
+		String search = searchByField.getText();
+		flightTable.getItems().clear();
+		
+		try {
+			Connection c = DBConnect.connect();
+			
+			ResultSet rs = c.createStatement().executeQuery("SELECT * from AirwaysData.flight WHERE departureTime LIKE '"+ search+"%'");
+			
+			while (rs.next()) {
+				flightList.add(new Flight(rs.getInt("flightID"), 
+						rs.getString("departureCity"), rs.getString("arrivalCity"), 
+						rs.getTime("departureTime"), rs.getTime("arrivalTime")));
+			}
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+		col_flightID.setCellValueFactory(new PropertyValueFactory<>("id"));
+		col_DepCity.setCellValueFactory(new PropertyValueFactory<>("depCity"));
+		col_ArrCity.setCellValueFactory(new PropertyValueFactory<>("arrCity"));
+		col_DepTime.setCellValueFactory(new PropertyValueFactory<>("depTime"));
+		col_ArrTime.setCellValueFactory(new PropertyValueFactory<>("arrTime"));
+		
+		flightTable.setItems(flightList);
+		
+	}
+	
+	@FXML
+	void searchByArrivalCity(ActionEvent event) {
+		String search = searchByField.getText();
+		flightTable.getItems().clear();
+		
+		try {
+			Connection c = DBConnect.connect();
+			
+			ResultSet rs = c.createStatement().executeQuery("SELECT * from AirwaysData.flight WHERE arrivalCity LIKE '"+ search+"%'");
+			
+			while (rs.next()) {
+				flightList.add(new Flight(rs.getInt("flightID"), 
+						rs.getString("departureCity"), rs.getString("arrivalCity"), 
+						rs.getTime("departureTime"), rs.getTime("arrivalTime")));
+			}
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+		col_flightID.setCellValueFactory(new PropertyValueFactory<>("id"));
+		col_DepCity.setCellValueFactory(new PropertyValueFactory<>("depCity"));
+		col_ArrCity.setCellValueFactory(new PropertyValueFactory<>("arrCity"));
+		col_DepTime.setCellValueFactory(new PropertyValueFactory<>("depTime"));
+		col_ArrTime.setCellValueFactory(new PropertyValueFactory<>("arrTime"));
+		
+		flightTable.setItems(flightList);
+		
+	}
+	
+	@FXML
+	void searchByArrivalTime(ActionEvent event) {
+		String search = searchByField.getText();
+		flightTable.getItems().clear();
+		
+		try {
+			Connection c = DBConnect.connect();
+			
+			ResultSet rs = c.createStatement().executeQuery("SELECT * from AirwaysData.flight WHERE arrivalTime LIKE '"+ search+"%'");
+			
+			while (rs.next()) {
+				flightList.add(new Flight(rs.getInt("flightID"), 
+						rs.getString("departureCity"), rs.getString("arrivalCity"), 
+						rs.getTime("departureTime"), rs.getTime("arrivalTime")));
+			}
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+		col_flightID.setCellValueFactory(new PropertyValueFactory<>("id"));
+		col_DepCity.setCellValueFactory(new PropertyValueFactory<>("depCity"));
+		col_ArrCity.setCellValueFactory(new PropertyValueFactory<>("arrCity"));
+		col_DepTime.setCellValueFactory(new PropertyValueFactory<>("depTime"));
+		col_ArrTime.setCellValueFactory(new PropertyValueFactory<>("arrTime"));
+		
+		flightTable.setItems(flightList);
 		
 	}
 
